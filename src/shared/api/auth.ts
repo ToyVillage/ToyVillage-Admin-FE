10951d8d1416d44d48514b787a862cc9e4d6f6c6
yunho09 <jyh19310302@gmail.com>
2026-07-28 21:14:30 +0900

    fix: token 관련 하드코딩 수정

--- src/shared/api/auth.ts
+++ src/shared/api/auth.ts
@@ -2,8 +2,7 @@ import { api } from '@/shared/api/axios'
 
 export function configureApiAuthentication(): void {
   api.interceptors.request.use((config) => {
-    const accessToken =
-      'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwidHlwZSI6ImFjY2VzcyIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTc4NTIzNTcwMCwiZXhwIjoxNzg1MjM5MzAwfQ.FzPhOMZRUAbbYU5K1T2DhvRRqxXVFtMYLtB09MMvQhgQwno0SFU4hOQUUB75G36SXcFue-n3Wogjitp7P0T7RA'
+    const accessToken = localStorage.getItem('accessToken')
 
     if (accessToken) {
       config.headers.set('Authorization', `Bearer ${accessToken}`)
