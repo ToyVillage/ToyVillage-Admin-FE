aa6f66615ca20312fd34411e1400d543aafcfd8c
yunho09 <jyh19310302@gmail.com>
2026-07-28 15:40:23 +0900

    fix: access token 임시 수정

--- src/shared/api/auth.ts
+++ src/shared/api/auth.ts
@@ -2,8 +2,7 @@ import { api } from '@/shared/api/axios'
 
 export function configureApiAuthentication(): void {
   api.interceptors.request.use((config) => {
-    const accessToken =
-      'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwidHlwZSI6ImFjY2VzcyIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTc4NTIxNTk2OSwiZXhwIjoxNzg1MjE5NTY5fQ.91qFBYZf14NFy04g_PT-009tZqonD71sixjtsaUhErlDtDb5sWzjtqX9wU0niGLUkf1VVoetq7iLiM9s57vkaA'
+    const accessToken = window.localStorage.getItem('accessToken')
 
     if (accessToken) {
       config.headers.set('Authorization', `Bearer ${accessToken}`)
